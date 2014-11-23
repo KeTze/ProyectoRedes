9dd82f53d52e5440ea8939e33be4aefce8e82a91
NeoDhaos27 <jorge-rego@opendeusto.es>
2014-11-23 12:52:38 +0100

    LoginWindow
    Pequeña ventana para logear (provisional).

---
+++ Client/src/tcpClient/LoginWindow.java
@@ -0,0 +1,204 @@
+package tcpClient;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JPasswordField;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public class LoginWindow extends JFrame{
+	
+	private JButton btnLogin;
+	private JPasswordField passwordField;
+	private JTextField txtUsername;
+	private int i;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow window = new LoginWindow();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginWindow() {
+		super();
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setBounds(100, 100, 324, 179);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		getContentPane().setBackground(Color.DARK_GRAY);
+		setResizable(false);
+		setTitle("Tralala Login");
+		setAlwaysOnTop(true);
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setForeground(Color.WHITE);
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		btnLogin = new JButton("Login");
+		btnLogin.setBackground(new Color(0.0f, 0.0f, 0.0f, 0.0f));
+		btnLogin.setForeground(Color.BLACK);
+		btnLogin.setOpaque(false);
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		passwordField.setEchoChar('*');
+		passwordField.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordField.setToolTipText("password");
+		
+		txtUsername = new JTextField();
+		txtUsername.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtUsername.setHorizontalAlignment(SwingConstants.CENTER);
+		txtUsername.setToolTipText("username");
+		txtUsername.setColumns(10);
+		
+		JLabel lblUser = new JLabel("User:");
+		lblUser.setForeground(Color.WHITE);
+		
+		JLabel lblPass = new JLabel("Pass:");
+		lblPass.setForeground(Color.WHITE);
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnLogin, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(11)
+							.addComponent(lblUser, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblPass)))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(passwordField, GroupLayout.DEFAULT_SIZE, 241, Short.MAX_VALUE)
+						.addComponent(txtUsername, GroupLayout.DEFAULT_SIZE, 241, Short.MAX_VALUE))
+					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblLogin, GroupLayout.DEFAULT_SIZE, 318, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(lblLogin, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblUser, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(txtUsername, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))
+					.addGap(7)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblPass)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnLogin, GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
+					.addGap(5))
+		);
+		getContentPane().setLayout(groupLayout);
+		
+		setLocationRelativeTo(null);
+		
+		eventos();
+	}
+
+	private void eventos() {
+		/*
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(txtUsername.getText().length()>0 && new String(passwordField.getPassword()).length()>0){
+					TralalaClient client = new TralalaClient();
+					int status = client.login(txtUsername.getText(), new String(passwordField.getPassword()));
+					TralalaClient.log("Valor status: "+String.valueOf(status));
+					if(status==2){
+						new MainWindow().setVisible(true);
+						dispose();
+						client.do1();
+						client.do2();
+					}
+					else{
+						JOptionPane.showMessageDialog(getFrame(), "Incorrect user or password. Try again!", "Information", JOptionPane.INFORMATION_MESSAGE);
+					}
+				}
+				else{
+					JOptionPane.showMessageDialog(getFrame(), "Please fill all the fields", "Information", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		});
+		
+		passwordField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent key) {
+				Character[] c = {'#', '*', '$'};
+				int val = i%3;
+				passwordField.setEchoChar(c[val]);
+				i++;
+			}
+		});
+	*/
+	}
+	
+	private JFrame getFrame(){
+		return this;
+	}
+}
