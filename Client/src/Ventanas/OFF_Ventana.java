f4024846bb8872825caa6776cf2a66767ba89406
NeoDhaos27 <jorge-rego@opendeusto.es>
2014-11-30 12:32:32 +0100

    Possible ON & OFF
    Tenemos que preguntar si lo hacemos como el DTE o como su ejemplo

---
+++ Client/src/Ventanas/OFF_Ventana.java
@@ -0,0 +1,147 @@
+package Ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JPasswordField;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import tcpClient.TCPClient;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+
+public class OFF_Ventana extends JFrame{
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow window = new LoginWindow();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public OFF_Ventana() {
+		super();
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setBounds(100, 100, 338, 238);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		getContentPane().setBackground(Color.DARK_GRAY);
+		setResizable(false);
+		setTitle("Control de Invernadero");
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JComboBox comboBox = new JComboBox();
+		
+		JLabel lblPlaca = new JLabel("Placa:");
+		lblPlaca.setForeground(Color.WHITE);
+		
+		JLabel lblVariable = new JLabel("Variable:");
+		lblVariable.setForeground(Color.WHITE);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblVariable)
+							.addGap(29))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblPlaca, GroupLayout.DEFAULT_SIZE, 67, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(textField)
+						.addComponent(comboBox, 0, 233, Short.MAX_VALUE))
+					.addContainerGap(14, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(46)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPlaca))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblVariable)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(88, Short.MAX_VALUE))
+		);
+		getContentPane().setLayout(groupLayout);
+		
+		setLocationRelativeTo(null);
+		
+		eventos();
+	}
+	private void desactivarBotones(){
+		
+	}
+	private void eventos() {
+		
+	
+	}
+	
+	private JFrame getFrame(){
+		return this;
+	}
+}
